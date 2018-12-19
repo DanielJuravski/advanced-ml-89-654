4571a526bd3bf4e66004989d93941351b79a911d
eyalorbach <eyal@mac.com>
2018-12-19 22:51:59 +0200

    viterbi looks a little better

--- ex2/biagram_perceptron.py
+++ ex2/biagram_perceptron.py
@@ -10,19 +10,21 @@ LR = 1
 
 def init_perceptron(x_size, y_size, vocab_size):
     eps = np.sqrt(6) / np.sqrt(x_size + y_size)
-    size = (vocab_size * x_size) + (y_size * vocab_size)
+    size = (y_size * x_size) + (vocab_size * y_size)
     w = np.random.uniform(-eps, eps, size)
-    b = np.random.uniform(-eps, eps, vocab_size)
+    b = np.random.uniform(-eps, eps, y_size)
     return (w,b)
 
 
 def phi(x, y_cand, y_size, vocab_size, prev_y):
     x_size = len(x)
-    size = (vocab_size * x_size) + (vocab_size * y_size)
+    size = (y_size * x_size) + (vocab_size * y_size)
     phi_xyprevy = np.zeros(size)
     x_start = y_cand*x_size
     phi_xyprevy[x_start:x_start+x_size] = x
-    prevy_start = y_cand*vocab_size
+
+    # prevy_start = y_cand*vocab_size
+    prevy_start= (y_size * x_size) + (y_cand * y_size)
     phi_xyprevy[prevy_start + prev_y] = 1
     return phi_xyprevy
 
@@ -67,7 +69,7 @@ def train_perceptron(perceptron, data_by_index, y_size, vocab_size):
                 else:
                     prev_y = utils.L2I(prev_example[utils.LETTER])
 
-            phis = [phi(x, y_cand, y_size, vocab_size, prev_y) for y_cand in range(vocab_size)]
+            phis = [phi(x, y_cand, y_size, vocab_size, prev_y) for y_cand in range(y_size)]
             results = [np.dot(w, phi_xy)  for i, phi_xy in enumerate(phis)]
             y_hat = int(np.argmax(results))
             w = w + (LR * (phis[y] - phis[y_hat]))
@@ -88,57 +90,75 @@ def train_perceptron(perceptron, data_by_index, y_size, vocab_size):
     return (w, b)
 
 
-def test(perceptron, test_data, y_size, vocab_size):
+def predict_word(perceptron, word_lines, y_size, vocab_size):
     w, b = perceptron
     good = bad = 0.0
-    for ex_i, example in enumerate(test_data):
-        print("predicting line %d from %d" %(ex_i, len(test_data)))
-        x = np.array(example[utils.PIXEL_VECTOR])
-        y = utils.L2I(example[utils.LETTER])
+    y_word=""
 
-        D_S = np.zeros((y_size,vocab_size))
-        D_PI = np.zeros((y_size,vocab_size))
+    D_S = np.zeros((len(word_lines),vocab_size))
+    D_PI = np.zeros((len(word_lines),vocab_size))
 
-        prev_char = utils.L2I('$') #get scores for the first letter where the perv is $
-        #initalization
-        for i in range(0,vocab_size):
-            curr_char = i+1
-            phi_xy = phi(x, curr_char, y_size, vocab_size, prev_char)
-            s = np.dot(w, phi_xy)
-            D_S[0][i] = s
-            D_PI[0][i] = 0
-        #// ===================== RECURSION ===================== //
-        for i in range(1, y_size):
-            for j in range(vocab_size):
-                curr_char = j+1
-                phis = [phi(x, curr_char, y_size, vocab_size, prev_cand) for prev_cand in range(vocab_size)]
-                results = [np.dot(w, phi_xy) + D_S[i-1][p_i] for p_i, phi_xy in enumerate(phis)]
-                i_best = int(np.argmax(results))
-                d_best = results[i_best]
-                D_S[i][j] = d_best
-                D_PI[i][j] = i_best
+    #initalization
+    prev_char = utils.L2I('$') #get scores for the first letter where the perv is $
+    x = np.array(word_lines[0][utils.PIXEL_VECTOR])
+    for i in range(y_size):
+        curr_char = i
+        phi_xy = phi(x, curr_char, y_size, vocab_size, prev_char)
+        s = np.dot(w, phi_xy)
+        D_S[0][i] = s
+        D_PI[0][i] = 26
+    #// ===================== RECURSION ===================== //
+    for i in range(1, len(word_lines)):
+        for j in range(y_size):
+            curr_char = j
+            phis = [phi(word_lines[i][utils.PIXEL_VECTOR], curr_char, y_size, vocab_size, prev_cand) for prev_cand in range(y_size)]
+            results = [np.dot(w, phi_xy) + D_S[i-1][p_i] for p_i, phi_xy in enumerate(phis)]
+            i_best = int(np.argmax(results))
+            d_best = results[i_best]
+            D_S[i][j] = d_best
+            D_PI[i][j] = i_best
 
-        #// ======================== BACK-TRACK ======================== //
-        y_hat = np.zeros(y_size)
-        d_best = -1
-        for i in range(vocab_size):
-            if d_best < D_S[y_size-1][i]:
-                y_hat[y_size-1] = i
-                d_best = D_S[y_size-1][i]
+    #// ======================== BACK-TRACK ======================== //
+    y_hat = np.zeros(len(word_lines))
+    d_best = -1
+    for i in range(y_size):
+        if d_best < D_S[len(word_lines)-1][i]:
+            y_hat[len(word_lines)-1] = i
+            d_best = D_S[len(word_lines)-1][i]
 
-        for i in range(y_size-2, 0, -1):
-            y_hat[i] = D_PI[i + 1][int(y_hat[i + 1])]
+    for i in range(len(word_lines)-2, -1, -1):
+        y_hat[i] = D_PI[i + 1][int(y_hat[i + 1])]
 
-        pred = int(np.argmax(y_hat))
+    for i, line in enumerate(word_lines):
+        pred = int(y_hat[i])
+        y = utils.L2I(line[utils.LETTER])
+        y_word +=line[utils.LETTER]
         print("pred=%d true=%d " %(pred, y))
         if y != pred:
             bad += 1
         else:
             good += 1
+    print("word was " + y_word)
+    return (good, bad)
+
+
+def test(perceptron, test_data, y_size, vocab_size):
+    good = bad = 0.0
+    word_lines = []
+    for ex_i, line in enumerate(test_data):
+        print("predicting line %d from %d" % (ex_i, len(word_lines)))
+        if line[utils.NEXT_ID] == -1:
+            word_lines.append(line)
+            w_good, w_bad = predict_word(perceptron, word_lines, y_size, vocab_size)
+            good += w_good
+            bad += w_bad
+            word_lines = []
+        else:
+            word_lines.append(line)
+
 
     perc = (good / (good + bad)) * 100
     print("==========Test accuracy = %.2f==========" % perc)
-    return perc
 
 
 def fill_data_by_index(train_data):
@@ -152,12 +172,12 @@ if __name__ == '__main__':
     x_size = 8*16
     y_size = 26
     vocab_size = 27
-    utils.ALPHABET = "$abcdefghijklmnopqrstuvwxyz"
+    # utils.ALPHABET = "$abcdefghijklmnopqrstuvwxyz"
     train_data = utils.load_data("data/letters.train.data")
     data_by_index = fill_data_by_index(train_data)
 
     perceptron = init_perceptron(x_size, y_size, vocab_size)
     perceptron = train_perceptron(perceptron, data_by_index, y_size, vocab_size)
     test_data = utils.load_data("data/letters.test.data")
-    shrinked_test_data = word_shuffle(test_data, 5)
+    shrinked_test_data = word_shuffle(test_data, 20)
     test_acc = test(perceptron, shrinked_test_data, y_size, vocab_size)
