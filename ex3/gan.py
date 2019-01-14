1dcaaac6e5040bc9f709f7a39edc3e1ffc8ab6d3
user <juravski95@gmail.com>
2019-01-14 20:37:12 +0200

    line works ?

--- ex3/gan.py
+++ ex3/gan.py
@@ -1,223 +1,205 @@
-#!/usr/bin/env python
-
-#  Generative Adversarial Networks (GAN) example in PyTorch. Tested with PyTorch 0.4.1, Python 3.6.7 (Nov 2018)
-# See related blog post at https://medium.com/@devnag/generative-adversarial-networks-gans-in-50-lines-of-code-pytorch-e81b79659e3f#.sch4xgsa9
-
 import numpy as np
+from matplotlib import pyplot as plt
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.autograd import Variable
+from torch import nn, optim
+from torch.autograd.variable import Variable
+from time import gmtime, strftime
 
-matplotlib_is_available = True
-try:
-  from matplotlib import pyplot as plt
-except ImportError:
-  print("Will skip plotting; matplotlib is not available.")
-  matplotlib_is_available = False
 
-# Data params
-data_mean = 4
-data_stddev = 1.25
-
-# ### Uncomment only one of these to define what data is actually sent to the Discriminator
-#(name, preprocess, d_input_func) = ("Raw data", lambda data: data, lambda x: x)
-#(name, preprocess, d_input_func) = ("Data and variances", lambda data: decorate_with_diffs(data, 2.0), lambda x: x * 2)
-#(name, preprocess, d_input_func) = ("Data and diffs", lambda data: decorate_with_diffs(data, 1.0), lambda x: x * 2)
-(name, preprocess, d_input_func) = ("Only 4 moments", lambda data: get_moments(data), lambda x: 4)
-
-print("Using data [%s]" % (name))
-
-# ##### DATA: Target data and generator input data
-def get_line_sampler():
-    x = np.random.uniform()
+def get_line_data(n):
+    # n number of samples to make
+    x = np.random.randn(n, 1)
     y = x
-    return torch.Tensor(x, y)
+
+    x /= x.max()
+    y /= y.max()
+
+    data = np.hstack((x, y))
+    data_tensor = torch.Tensor(data)
+
+    return data_tensor
 
 
-def get_distribution_sampler(mu, sigma):
-    return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))  # Gaussian
+def get_rand_data(n):
+    # n number of samples to make
+    x = np.random.uniform(size=(n, 1))
+    y = np.random.uniform(size=(n, 1))
+    #x = np.random.randn(n, 1)
+    #y = np.random.randn(n, 1)
 
-def get_generator_input_sampler():
-    return lambda m, n: torch.rand(m, n)  # Uniform-dist data into generator, _NOT_ Gaussian
+    #x /= x.max()
+    #y /= y.max()
 
-# ##### MODELS: Generator model and discriminator model
+    data = np.hstack((x, y))
+    data_tensor = Variable(torch.Tensor(data))
 
-class Generator(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, f):
-        super(Generator, self).__init__()
-        self.map1 = nn.Linear(input_size, hidden_size)
-        self.map2 = nn.Linear(hidden_size, hidden_size)
-        self.map3 = nn.Linear(hidden_size, output_size)
-        self.f = f
+    return data_tensor
+
+
+class DiscriminatorNet(torch.nn.Module):
+    """
+    A three hidden-layer discriminative neural network
+    """
+
+    def __init__(self):
+        super(DiscriminatorNet, self).__init__()
+        n_features = 2
+        n_out = 2
+
+        self.hidden0 = nn.Linear(n_features, 5)
+        self.hidden1 = nn.Linear(5, 10)
+        self.out = nn.Linear(10, n_out)
+        self.f = torch.sigmoid
+
 
     def forward(self, x):
-        x = self.map1(x)
-        x = self.f(x)
-        x = self.map2(x)
-        x = self.f(x)
-        x = self.map3(x)
+        x = self.f(self.hidden0(x))
+        x = self.f(self.hidden1(x))
+        x = self.f(self.out(x))
+
         return x
 
-class Discriminator(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, f):
-        super(Discriminator, self).__init__()
-        self.map1 = nn.Linear(input_size, hidden_size)
-        self.map2 = nn.Linear(hidden_size, hidden_size)
-        self.map3 = nn.Linear(hidden_size, output_size)
-        self.f = f
+
+class GeneratorNet(torch.nn.Module):
+    """
+    A three hidden-layer generative neural network
+    """
+
+    def __init__(self):
+        super(GeneratorNet, self).__init__()
+        n_features = 2
+        n_out = 2
+
+        self.hidden0 = nn.Linear(n_features, 5)
+        self.hidden1 = nn.Linear(5, 10)
+        self.out = nn.Linear(10, n_out)
+        self.f = torch.tanh
 
     def forward(self, x):
-        x = self.f(self.map1(x))
-        x = self.f(self.map2(x))
-        return self.f(self.map3(x))
+        x = self.f(self.hidden0(x))
+        x = self.f(self.hidden1(x))
+        x = self.f(self.out(x))
 
-def extract(v):
-    return v.data.storage().tolist()
-
-def stats(d):
-    return [np.mean(d), np.std(d)]
-
-def get_moments(d):
-    # Return the first 4 moments of the data provided
-    mean = torch.mean(d)
-    diffs = d - mean
-    var = torch.mean(torch.pow(diffs, 2.0))
-    std = torch.pow(var, 0.5)
-    zscores = diffs / std
-    skews = torch.mean(torch.pow(zscores, 3.0))
-    kurtoses = torch.mean(torch.pow(zscores, 4.0)) - 3.0  # excess kurtosis, should be 0 for Gaussian
-    final = torch.cat((mean.reshape(1,), std.reshape(1,), skews.reshape(1,), kurtoses.reshape(1,)))
-    return final
-
-def decorate_with_diffs(data, exponent, remove_raw_data=False):
-    mean = torch.mean(data.data, 1, keepdim=True)
-    mean_broadcast = torch.mul(torch.ones(data.size()), mean.tolist()[0][0])
-    diffs = torch.pow(data - Variable(mean_broadcast), exponent)
-    if remove_raw_data:
-        return torch.cat([diffs], 1)
-    else:
-        return torch.cat([data, diffs], 1)
-
-def train():
-    # Model parameters
-    g_input_size = 2      # Random noise dimension coming into generator, per output vector
-    g_hidden_size = 5     # Generator complexity
-    g_output_size = 2     # Size of generated output vector
-
-    d_input_size = 500    # Minibatch size - cardinality of distributions
-    d_hidden_size = 10    # Discriminator complexity
-    d_output_size = 1     # Single dimension for 'real' vs. 'fake' classification
-    minibatch_size = d_input_size
-
-    d_learning_rate = 1e-3
-    g_learning_rate = 1e-3
-    sgd_momentum = 0.9
-
-    num_epochs = 2000 # 5000
-    print_interval = 100
-    d_steps = 20
-    g_steps = 20
-
-    dfe, dre, ge = 0, 0, 0
-    d_real_data, d_fake_data, g_fake_data = None, None, None
-
-    discriminator_activation_function = torch.sigmoid
-    generator_activation_function = torch.tanh
-
-    #d_sampler = get_distribution_sampler(data_mean, data_stddev)
-    line_list = []
-    for _ in range(d_input_size):
-        x = np.random.randn()
-        y = x
-        line_list.append((x,y))
-    d_sampler = torch.Tensor(line_list)
-
-    rand_uniform_list = []
-    for _ in range(minibatch_size):
-        x = np.random.uniform()
-        y = np.random.uniform()
-        rand_uniform_list.append((x, y))
-    gi_sampler = torch.Tensor(rand_uniform_list)
+        return x
 
 
+def ones_target(size):
+    '''
+    Tensor containing ones, with shape = size
+    '''
+    data = Variable(torch.ones(size, 2))
+    return data
 
-    #gi_sampler = get_generator_input_sampler()
-    G = Generator(input_size=g_input_size,
-                  hidden_size=g_hidden_size,
-                  output_size=g_output_size,
-                  f=generator_activation_function)
-    D = Discriminator(input_size=d_input_func(d_input_size),
-                      hidden_size=d_hidden_size,
-                      output_size=d_output_size,
-                      f=discriminator_activation_function)
-    criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
-    d_optimizer = optim.SGD(D.parameters(), lr=d_learning_rate, momentum=sgd_momentum)
-    g_optimizer = optim.SGD(G.parameters(), lr=g_learning_rate, momentum=sgd_momentum)
 
-    for epoch in range(num_epochs):
-        for d_index in range(d_steps):
-            # 1. Train D on real+fake
-            D.zero_grad()
+def zeros_target(size):
+    '''
+    Tensor containing zeros, with shape = size
+    '''
+    data = Variable(torch.zeros(size, 2))
+    return data
 
-            #  1A: Train D on real
-            d_real_data = Variable(d_sampler)
-            d_real_decision = D(preprocess(d_real_data))
-            d_real_error = criterion(d_real_decision, Variable(torch.ones([1,1])))  # ones = true
-            d_real_error.backward() # compute/store gradients, but don't change params
 
-            #  1B: Train D on fake
-            d_gen_input = Variable(gi_sampler)
-            d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
-            d_fake_decision = D(preprocess(d_fake_data.t()))
-            d_fake_error = criterion(d_fake_decision, Variable(torch.zeros([1,1])))  # zeros = fake
-            d_fake_error.backward()
-            d_optimizer.step()     # Only optimizes D's parameters; changes based on stored gradients from backward()
+def train_discriminator(optimizer):
+    d_range = 20
+    for i in range(d_range):
+        line_data = get_line_data(batch_size)
+        test_noise = get_rand_data(batch_size)
+        real_data = Variable(line_data)
+        # Generate fake data and detach (so gradients are not calculated for generator)
+        fake_data = generator(test_noise).detach()
+        N = real_data.size(0)
+        # Reset gradients
+        optimizer.zero_grad()
 
-            dre, dfe = extract(d_real_error)[0], extract(d_fake_error)[0]
+        # 1.1 Train on Real Data
+        prediction_real = discriminator(real_data)
+        # Calculate error and backpropagate
+        error_real = loss(prediction_real, ones_target(N))
+        error_real.backward()
 
-        for g_index in range(g_steps):
-            # 2. Train G on D's response (but DO NOT train D on these labels)
-            G.zero_grad()
+        # 1.2 Train on Fake Data
+        prediction_fake = discriminator(fake_data)
+        # Calculate error and backpropagate
+        error_fake = loss(prediction_fake, zeros_target(N))
+        error_fake.backward()
 
-            gen_input = Variable(gi_sampler)
-            g_fake_data = G(gen_input)
-            dg_fake_decision = D(preprocess(g_fake_data.t()))
-            g_error = criterion(dg_fake_decision, Variable(torch.ones([1,1])))  # Train G to pretend it's genuine
+        # 1.3 Update weights with gradients
+        optimizer.step()
 
-            g_error.backward()
-            g_optimizer.step()  # Only optimizes G's parameters
-            ge = extract(g_error)[0]
+    # Return error and predictions for real and fake inputs
+    return error_real + error_fake, prediction_real, prediction_fake
 
-        if epoch % print_interval == 0:
-            print("Epoch %s: D (%s real_err, %s fake_err) G (%s err); Real Dist (%s),  Fake Dist (%s) " %
-                  (epoch, dre, dfe, ge, stats(extract(d_real_data)), stats(extract(d_fake_data))))
 
-    g_list = g_fake_data.data.storage().tolist()
-    X_hat = [[g_list[0::2][i]] for i in range(minibatch_size)]
-    Y_hat = [[g_list[1::2][i]] for i in range(minibatch_size)]
+def train_generator(optimizer):
+    # range beyond 1 works bad
+    g_range = 1
+    for i in range(g_range):
+        test_noise = get_rand_data(batch_size)
+        fake_data = generator(test_noise)
+        N = fake_data.size(0)
+        # Reset gradients
+        optimizer.zero_grad()
 
-    #X_hat /= X_hat.max()
-    #Y_hat /= Y_hat.max()
-    data_hat = np.hstack((X_hat, Y_hat))
+        # Sample noise and generate fake data
+        prediction = discriminator(fake_data)
 
-    X = data_hat
-    plt.plot(X[:, 0], X[:, 1], '.', label='class 1')
+        # Calculate error and backpropagate
+        error = loss(prediction, ones_target(N))
+        error.backward()
+
+        # Update weights with gradients
+        optimizer.step()
+
+    # Return error
+    return error
+
+
+def show_plot(data, batch_size):
+    data = data.data.storage().tolist()
+    X_hat = np.array([[data[0::2][i]] for i in range(batch_size)])
+    Y_hat = np.array([[data[1::2][i]] for i in range(batch_size)])
+
+    data_con = np.hstack((X_hat, Y_hat))
+
+    plt.plot(data_con[:, 0], data_con[:, 1], '.')
     plt.title('training set')
-    plt.legend()
+    plt.savefig('output')
     plt.show()
 
-    # if matplotlib_is_available:
-    #     print("Plotting the generated distribution...")
-    #     values = extract(g_fake_data)
-    #     print(" Values: %s" % (str(values)))
-    #     plt.hist(values, bins=50)
-    #     plt.xlabel('Value')
-    #     plt.ylabel('Count')
-    #     plt.title('Histogram of Generated Distribution')
-    #     plt.grid(True)
-    #     plt.show()
+
+if __name__ == '__main__':
+    print("Start time: " + strftime("%H:%M:%S", gmtime()))
+    batch_size = 50
+    line_data = get_line_data(batch_size)
+    #show_plot(line_data, batch_size)
+    discriminator = DiscriminatorNet()
+    generator = GeneratorNet()
+    sgd_momentum = 0.9
+    d_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002)
+    g_optimizer = optim.Adam(generator.parameters(), lr=0.0002)
+    loss = nn.BCELoss()
+
+    num_epochs = 6000
+    for epoch in range(num_epochs):
+        # Train Discriminator
+        d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer)
+
+        # Train Generator
+        g_error = train_generator(g_optimizer)
+
+        # Display Progress
+        if (epoch) % 100 == 0 and epoch > 1:
+            print('Epoch: ' + str(epoch) +
+                  ' D_err: ' + str(d_error.data.storage().tolist()[0]) +
+                  #' D_pred_real: ' + str(d_pred_real) +
+                  #' D_pred_fake: ' + str(d_pred_fake) +
+                  ' G_err: ' + str(g_error.data.storage().tolist()[0]))
+            pass
+        if (epoch) % 1000 == 0 and epoch > 1:
+            test_noise = get_rand_data(batch_size)
+            fake_data = generator(test_noise)
+            show_plot(fake_data, batch_size)
+            pass
 
 
-train()
-
+    print("End time: " + strftime("%H:%M:%S", gmtime()))
