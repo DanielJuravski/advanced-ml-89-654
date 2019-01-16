8f6e795b89cd1635fceb09694a264cba274244b3
user <juravski95@gmail.com>
2019-01-16 11:34:40 +0200

    some beauty fixes

--- ex3/gan.py
+++ ex3/gan.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn, optim
 from torch.autograd.variable import Variable
 from time import gmtime, strftime
+import sys
 
 
 def get_line_data(n):
@@ -18,6 +19,24 @@ def get_line_data(n):
     data_tensor = torch.Tensor(data)
 
     return data_tensor
+
+
+def get_parabola_data(n):
+    # n number of samples to make
+    x = np.random.randn(n, 1)
+    y = x**2
+
+    x /= x.max()
+    y /= y.max()
+
+    data = np.hstack((x, y))
+    data_tensor = torch.Tensor(data)
+
+    return data_tensor
+
+
+def get_spiral_data(n):
+    pass
 
 
 def get_rand_data(n):
@@ -40,7 +59,6 @@ class DiscriminatorNet(torch.nn.Module):
     """
     A three hidden-layer discriminative neural network
     """
-
     def __init__(self):
         super(DiscriminatorNet, self).__init__()
         n_features = 2
@@ -64,7 +82,6 @@ class GeneratorNet(torch.nn.Module):
     """
     A three hidden-layer generative neural network
     """
-
     def __init__(self):
         super(GeneratorNet, self).__init__()
         n_features = 2
@@ -74,6 +91,7 @@ class GeneratorNet(torch.nn.Module):
         self.hidden1 = nn.Linear(5, 10)
         self.out = nn.Linear(10, n_out)
         self.f = torch.tanh
+        self.dropout = nn.Dropout(0.0)
 
     def forward(self, x):
         x = self.f(self.hidden0(x))
@@ -102,25 +120,25 @@ def zeros_target(size):
 def train_discriminator(optimizer):
     d_range = 20
     for i in range(d_range):
-        line_data = get_line_data(batch_size)
-        test_noise = get_rand_data(batch_size)
-        real_data = Variable(line_data)
+        sample_data = get_real_data(DATA_TYPE)
+        real_data = Variable(sample_data(BATCH_SIZE))
+        noise = get_rand_data(BATCH_SIZE)
         # Generate fake data and detach (so gradients are not calculated for generator)
-        fake_data = generator(test_noise).detach()
-        N = real_data.size(0)
+        fake_data = generator(noise).detach()
+
         # Reset gradients
         optimizer.zero_grad()
 
         # 1.1 Train on Real Data
         prediction_real = discriminator(real_data)
         # Calculate error and backpropagate
-        error_real = loss(prediction_real, ones_target(N))
+        error_real = loss(prediction_real, ones_target(BATCH_SIZE))
         error_real.backward()
 
         # 1.2 Train on Fake Data
         prediction_fake = discriminator(fake_data)
         # Calculate error and backpropagate
-        error_fake = loss(prediction_fake, zeros_target(N))
+        error_fake = loss(prediction_fake, zeros_target(BATCH_SIZE))
         error_fake.backward()
 
         # 1.3 Update weights with gradients
@@ -134,9 +152,9 @@ def train_generator(optimizer):
     # range beyond 1 works bad
     g_range = 1
     for i in range(g_range):
-        test_noise = get_rand_data(batch_size)
-        fake_data = generator(test_noise)
-        N = fake_data.size(0)
+        noise = get_rand_data(BATCH_SIZE)
+        fake_data = generator(noise)
+
         # Reset gradients
         optimizer.zero_grad()
 
@@ -144,7 +162,7 @@ def train_generator(optimizer):
         prediction = discriminator(fake_data)
 
         # Calculate error and backpropagate
-        error = loss(prediction, ones_target(N))
+        error = loss(prediction, ones_target(BATCH_SIZE))
         error.backward()
 
         # Update weights with gradients
@@ -167,20 +185,38 @@ def show_plot(data, batch_size):
     plt.show()
 
 
+def get_real_data(data_type):
+    if data_type == 'line':
+        real_data_fun = get_line_data
+    elif data_type == 'parabola':
+        real_data_fun = get_parabola_data
+    elif data_type == 'spiral':
+        real_data_fun = get_spiral_data
+    else:
+        raise Exception('Wrong data type')
+
+    return real_data_fun
+
+
 if __name__ == '__main__':
     print("Start time: " + strftime("%H:%M:%S", gmtime()))
-    batch_size = 50
-    line_data = get_line_data(batch_size)
-    #show_plot(line_data, batch_size)
+
+    if len(sys.argv) > 2:
+        DATA_TYPE = sys.argv[1]
+        BATCH_SIZE = int(sys.argv[2])
+        EPOCHS = int(sys.argv[3])
+    else:
+        DATA_TYPE = 'parabola'
+        BATCH_SIZE = 20
+        EPOCHS = 40000
+
     discriminator = DiscriminatorNet()
     generator = GeneratorNet()
-    sgd_momentum = 0.9
     d_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002)
     g_optimizer = optim.Adam(generator.parameters(), lr=0.0002)
     loss = nn.BCELoss()
 
-    num_epochs = 6000
-    for epoch in range(num_epochs):
+    for epoch in range(EPOCHS):
         # Train Discriminator
         d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer)
 
@@ -191,15 +227,9 @@ if __name__ == '__main__':
         if (epoch) % 100 == 0 and epoch > 1:
             print('Epoch: ' + str(epoch) +
                   ' D_err: ' + str(d_error.data.storage().tolist()[0]) +
-                  #' D_pred_real: ' + str(d_pred_real) +
-                  #' D_pred_fake: ' + str(d_pred_fake) +
                   ' G_err: ' + str(g_error.data.storage().tolist()[0]))
-            pass
-        if (epoch) % 1000 == 0 and epoch > 1:
-            test_noise = get_rand_data(batch_size)
-            fake_data = generator(test_noise)
-            show_plot(fake_data, batch_size)
-            pass
 
-
+    test_noise = get_rand_data(500)
+    fake_data = generator(test_noise)
+    show_plot(fake_data, 500)
     print("End time: " + strftime("%H:%M:%S", gmtime()))
